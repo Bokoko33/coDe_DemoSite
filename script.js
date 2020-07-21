070c22af12eb03415de44c40e3d03251d56253d8
Bokoko <kamabokoman1042@gmail.com>
2020-07-22 00:15:34 +0900

    スマホメニューのスクロール防止、バツ以外で閉じる処理

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ $(function () {
     if ($(".header-sp-icon-wrap").hasClass("is-open")) {
       $(".header-sp-icon-wrap").removeClass("is-open");
       $(".header-sp-nav").css("display", "none");
+
+      $("html,body").removeAttr("style");
+      $(window).off("touchmove.noscroll");
     } else {
       $(".header-sp-icon-wrap").addClass("is-open");
       $(".header-sp-nav").css("display", "block");
+
+      $("html,body").css("overflow", "hidden");
+      $(window).on("touchmove", function (event) {
+        event.preventDefault();
+      });
+    }
+  });
+
+  $(".header-sp-nav").click(function () {
+    if ($(".header-sp-icon-wrap").hasClass("is-open")) {
+      $(".header-sp-nav").css("display", "none");
+      $(".header-sp-icon-wrap").removeClass("is-open");
+
+      $("html,body").removeAttr("style");
+      $(window).off("touchmove.noscroll");
     }
   });
 });
